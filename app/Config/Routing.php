1e19a6ccbcace99d350e755694222161b5698a56
Sourav Kumar Patra <97288696+souravkumarpatra15@users.noreply.github.com>
2026-09-11 20:54:58 +0530

    Load isolated trial lead routes

--- app/Config/Routing.php
+++ app/Config/Routing.php
@@ -30,6 +30,7 @@ class Routing extends BaseRouting
      */
     public array $routeFiles = [
         APPPATH . 'Config/Routes.php',
+        APPPATH . 'Config/TrialLeadRoutes.php',
     ];
 
     /**
@@ -37,9 +38,9 @@ class Routing extends BaseRouting
      * The default namespace to use for Controllers when no other
      * namespace has been specified.
      *
-     * Default: 'App\Controllers'
+     * Default: 'App\\Controllers'
      */
-    public string $defaultNamespace = 'App\Controllers';
+    public string $defaultNamespace = 'App\\Controllers';
 
     /**
      * For Auto Routing.
@@ -52,8 +53,8 @@ class Routing extends BaseRouting
 
     /**
      * For Defined Routes and Auto Routing.
-     * The default method to call on the controller when no other
-     * method has been set in the route.
+     * The default method to call on the controller when no other method
+     * has been set in the route.
      *
      * Default: 'index'
      */
@@ -70,67 +71,34 @@ class Routing extends BaseRouting
 
     /**
      * Sets the class/method that should be called if routing doesn't
-     * find a match. It can be the controller/method name like: Users::index
-     *
-     * This setting is passed to the Router class and handled there.
-     *
-     * If you want to use a closure, you will have to set it in the
-     * routes file by calling:
-     *
-     * $routes->set404Override(function() {
-     *    // Do something here
-     * });
-     *
-     * Example:
-     *  public $override404 = 'App\Errors::show404';
+     * find a match. It can be the controller/method name like `Users::list`.
      */
     public ?string $override404 = null;
 
     /**
-     * If TRUE, the system will attempt to match the URI against
-     * Controllers by matching each segment against folders/files
-     * in APPPATH/Controllers, when a match wasn't found against
-     * defined routes.
-     *
-     * If FALSE, will stop searching and do NO automatic routing.
+     * If TRUE, the system will attempt to match the URI against Controllers
+     * when a match wasn't found against defined routes.
      */
     public bool $autoRoute = false;
 
     /**
-     * If TRUE, the system will look for attributes on controller
-     * class and methods that can run before and after the
-     * controller/method.
-     *
-     * If FALSE, will ignore any attributes.
+     * If TRUE, the system will look for attributes on controller class and
+     * methods that can run before and after the controller action.
      */
     public bool $useControllerAttributes = true;
 
     /**
      * For Defined Routes.
-     * If TRUE, will enable the use of the 'prioritize' option
-     * when defining routes.
-     *
-     * Default: false
      */
     public bool $prioritize = false;
 
     /**
      * For Defined Routes.
-     * If TRUE, matched multiple URI segments will be passed as one parameter.
-     *
-     * Default: false
      */
     public bool $multipleSegmentsOneParam = false;
 
     /**
      * For Auto Routing (Improved).
-     * Map of URI segments and namespaces.
-     *
-     * The key is the first URI segment. The value is the controller namespace.
-     * E.g.,
-     *   [
-     *       'blog' => 'Acme\Blog\Controllers',
-     *   ]
      *
      * @var array<string, string>
      */
@@ -138,12 +106,6 @@ class Routing extends BaseRouting
 
     /**
      * For Auto Routing (Improved).
-     * Whether to translate dashes in URIs for controller/method to CamelCase.
-     * E.g., blog-controller -> BlogController
-     *
-     * If you enable this, $translateURIDashes is ignored.
-     *
-     * Default: false
      */
     public bool $translateUriToCamelCase = true;
 }
